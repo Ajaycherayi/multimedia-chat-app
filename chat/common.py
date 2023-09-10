5b5587f4754e0b8c4782135edcce44168ebe8fee
Ajay c <cajaycs@gmail.com>
2023-09-10 20:16:02 +0530

    Remove unwanted code

--- chat/common.py
+++ chat/common.py
@@ -1,71 +1,6 @@
-import hashlib
-from Crypto.Cipher import AES
-from base64 import urlsafe_b64encode, urlsafe_b64decode
-
 from django.http import Http404
+from .models import LastMessage, Room, RoomSession, LastSeenMessage, Message, User
 
-from .models import LastMessage, Room, RoomSession, LastSeenMessage
-
-
-# urlsafe_b64encode encryption
-from hashlib import sha256
-import base64
-from Crypto import Random
-from Crypto.Cipher import AES
-
-from cryptography.fernet import Fernet
-from cryptography.hazmat.primitives import hashes
-from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
-from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
-import os
-
-class AESRoomEnc(object):
-    iv = 'a2xhcgAAAAAAAAAA'
-    front_key = b'Zx*}kn@bf>(sX8Ep'
-    back_key = b'TrVMD/+_mG(j76,8'
-    def __init__(self, key): 
-        self.bs = 16
-        cryption_key = "%^&+@=$-%#-#@"+ key +"+^&_-!@#&^&="
-        self.key = hashlib.md5(cryption_key.encode()).digest().hex().upper()
-    def encrypt(self, raw):
-        #append strings to both end
-        plain_text = self.front_key + raw + self.back_key
-        plain_text = self._pad(plain_text.decode('utf-8'))
-
-        cipher = AES.new(self.key, AES.MODE_CBC, self.iv)
-        encrypt = urlsafe_b64encode(cipher.encrypt(plain_text))
-        return encrypt
-
-    def decrypt(self, enc):
-        # Try catch added for throwing incorrect padding exception
-        try:
-            enc = urlsafe_b64decode(enc)
-            cipher = AES.new(self.key, AES.MODE_CBC, self.iv)
-            decrypt = self._unpad(cipher.decrypt(enc)).decode('utf-8')
-            decoded_front_key = decrypt[:16]
-            decoded_back_key = decrypt[-16:]
-            # check these string matches our key, if matches remove these strings from the decoded string
-            if decoded_front_key == self.front_key and decoded_back_key == self.back_key:
-                # remove first 16 char
-                decrypt =  decrypt[16:]
-                # remove end 16 char
-                decrypt =  decrypt[:-16]
-            return decrypt
-        except Exception as e:
-            pp(e)
-            return ""
-    def _pad(self, s):
-        return s + (self.bs - len(s) % self.bs) * chr(self.bs - len(s) % self.bs)
-
-    @staticmethod
-    def _unpad(s):        
-        return s[:-ord(s[len(s)-1:])]
-
-
-
-# -------------------------------------------#
-#               Room Functions               #
-# -------------------------------------------#
 
 def exist_user_in_room(user,room):
     if Room.objects.filter(participants=user.id,name=room).exists():
@@ -78,6 +13,23 @@ def exist_user_in_roomId(user,room):
         return True
     else:
         return Http404
+
+def get_room_chat(room):
+    context = {}
+    try:
+        room_chat = Message.objects.filter(room=room).order_by('timestamp')
+        if room_chat:
+            participents = list(room_chat.values_list('sender_id', flat=True))
+            participentsData = { x['id']:{'profile_image':'{% static  "img/user-1.png" %}','name':x['name']} for x in User.objects.filter(id__in = participents).values('name','id')}
+            roomChat = { x['id']:{'message':x["content"],'time':x['timestamp'],'sender':x['sender_id']} for x in room_chat.values('id','sender_id','timestamp','content')}
+            context['room_participents'] = participentsData
+            context['room_chat'] = roomChat
+        else:
+            context['error'] = True
+    except Exception as e:
+        print("Exception from get_room_chat",e)
+        
+    return context
 
 def set_room_session(user,room):
     obj, created = RoomSession.objects.get_or_create( user_id=user.id,
--- chat/views.py
+++ chat/views.py
@@ -1,11 +1,10 @@
-import base64
 from django.shortcuts import render,redirect
 from django.urls import reverse
 from django.views import View
 from django.http import Http404, JsonResponse
 from django.views import View
 from django.shortcuts import render
-from chat.common import update_room_last_message, exist_user_in_room, set_room_session,exist_user_in_roomId,exist_room,update_last_seen_message
+from chat.common import update_room_last_message, exist_user_in_room, set_room_session,exist_user_in_roomId,exist_room,update_last_seen_message,get_room_chat
 from chat.models import LastMessage, LastSeenMessage, Message, Room,User
 
 ENC_KEY = "M^b%WX!4tE{NFd%-DcAT6Lj"
@@ -18,27 +17,24 @@ class JoinRoom(View):
     def post(self,request, *args, **kwargs):
         active_room = request.POST.get('room',None)
         username = request.POST.get('username',None)
-        if username :
-            user = User.objects.filter(name = username).first()
-            if user is None:
-                ints = User()
-                ints.name = username
-                ints.save()
-                user = ints
-            
-            if active_room:
-                print('User',user.id)
-                if exist_user_in_room(user,active_room) is not True:
-                    if exist_room(active_room):
-                        ints = Room.objects.get(name = active_room)
-                        ints.participants.add(user)
-                    else:
-                        ints = Room()
-                        ints.name = active_room
-                        ints.save()
-                        ints.participants.add(user)
-                return redirect(reverse(viewname='chatroom_view',kwargs={'user':user.name}))
-        return render(request ,self.template_name,{})
+        user = User.objects.filter(name = username).first()
+        if user is None:
+            userInst = User()
+            userInst.name = username
+            userInst.save()
+            user = userInst
+        
+        if exist_user_in_room(user,active_room) is not True:
+            if exist_room(active_room):
+                roomInst = Room.objects.get(name = active_room)
+                roomInst.participants.add(user)
+            else:
+                roomInst = Room()
+                roomInst.name = active_room
+                roomInst.save()
+                roomInst.participants.add(user)
+                
+        return redirect(reverse(viewname='chatroom_view',kwargs={'user':user.name}))
 
 
 class ChatRoomView(View):
@@ -46,73 +42,47 @@ class ChatRoomView(View):
     def get(self,request,*args,**kwargs):
         
         context = {}
-        user = None
-        if 'user' in kwargs:
-            user = kwargs['user']
-        if user:
-            ints = User.objects.get(name=user)
-            if ints:
-                active_room = request.GET.get('room',None)
-                if active_room is not None:
-                    context['active_room'] = active_room
-                    # room_id = AESRoomEnc(ENC_KEY).decrypt(active_room,ENC_KEY)
-                    room_id = active_room
-                    if exist_user_in_roomId(ints,room_id):
-                        set_room_session(ints,room_id)
-                        room_chat = Message.objects.filter(room=room_id).order_by('timestamp')
-                        if room_chat:
-                            update_last_seen_message(ints,room_id)
-                            participents = list(room_chat.values_list('sender_id', flat=True))
-                            participentsData = { x['id']:{'profile_image':"chat/static/img/users/user-3.png",'name':x['name']} for x in User.objects.filter(id__in = participents).values('name','id')}
-                            roomChat = { x['id']:{'message':x["content"],'time':x['timestamp'],'sender':x['sender_id']} for x in room_chat.values('id','sender_id','timestamp','content')}
-                            context['room_participents'] = participentsData
-                            context['room_chat'] = roomChat
-
-                rooms = Room.objects.filter(participants=ints.id)
-                if rooms:
+        user = kwargs.get('user',None)
+        active_room = request.GET.get('room',None)
+        userInst = User.objects.filter(name=user).first()
+        if userInst :
+            context['user_id'] = userInst.name
+            
+            # Get active chat data
+            if active_room and exist_user_in_roomId(userInst,active_room):
+                context['active_room'] = active_room
+                set_room_session(userInst,active_room) #set session
+                context.update(get_room_chat(active_room))
+                update_last_seen_message(userInst,active_room)
+                
+            # Get active Roomes
+            rooms = Room.objects.filter(participants=userInst.id)
+            if rooms:
+                roomIdList = list(rooms.values_list('id', flat=True))
+                msgIdList = list(LastMessage.objects.filter(room__in=roomIdList).values_list('message', flat=True))
+                lastMessages = { x['room_id']:{"time":x['timestamp'],"msg":x['content']} for x in Message.objects.filter(id__in=msgIdList).values('timestamp','content','room_id')}
+                lastViewMessage = dict(LastSeenMessage.objects.filter(room_id__in=roomIdList,user_id= userInst.id).values_list('room_id','message'))
+                
+                messages = {}
+                roomIdList = []
+                for e in rooms:
+                    encrtped_id = str(e.id)
+                    roomIdList.append(encrtped_id)
+                    count = ''
+                    if e.id in lastViewMessage and lastViewMessage[e.id]:
+                        count = Message.objects.filter(room_id = e.id,id__gt = lastViewMessage[e.id]).count()
+                    count = count if count != 0 else ''
+                    data = {
+                        "name": e.name,
+                        "message_count" : count,
+                    }
+                    if e.id in lastMessages:
+                        data['last_message'] = lastMessages[e.id]["msg"] if lastMessages[e.id]["msg"] else ''
+                        data['message_time'] = lastMessages[e.id]["time"] if lastMessages[e.id]["time"] else ''
+                    messages[encrtped_id] = data
                     
-                    roomIdList = list(rooms.values_list('id', flat=True))
-                    
-                    msgIdList = list(LastMessage.objects.filter(room__in=roomIdList).values_list('message', flat=True))
-                    lastMessages = { x['room_id']:{"time":x['timestamp'],"msg":x['content']} for x in Message.objects.filter(id__in=msgIdList).values('timestamp','content','room_id')}
-                    print("lastMessage--------s",lastMessages)
-                    lastViewMessage = dict(LastSeenMessage.objects.filter(room_id__in=roomIdList,user_id= ints.id).values_list('room_id','message'))
-                    print("lastMessage++++s",lastViewMessage)
-                    
-                    messages = {}
-                    roomIdList = []
-                    for e in rooms:
-                        # encrtped_id = AESRoomEnc(ENC_KEY).encrypt(str(e.id)).decode('utf-8')
-                        encrtped_id = str(e.id)
-                        # encrtped_id = encrypt_string(str(e.id)., ENC_KEY)
-                        roomIdList.append(encrtped_id)
-                        count = ''
-                            
-                        if e.id in lastViewMessage and lastViewMessage[e.id]:
-                            count = Message.objects.filter(room_id = e.id,id__gt = lastViewMessage[e.id]).count()
-                        count = count if count != 0 else ''
-                        data = {
-                            "name": e.name,
-                            "message_count" : count,
-                        }
-                        if e.id in lastMessages:
-                            data['last_message'] = lastMessages[e.id]["msg"] if lastMessages[e.id]["msg"] else ''
-                            data['message_time'] = lastMessages[e.id]["time"] if lastMessages[e.id]["time"] else ''
-
-                        messages[encrtped_id] = data
-                    context['room_id_list'] = roomIdList
-
-                    # pass encrtpted user id and it's key
-                    # key = str(int(time.time())) #only string value accepted
-                    # user_id = str(request.user.id) if request.user.id else '0'
-                    # crypt = AESCipher(key)
-                    # encrtped_id = crypt.encrypt(user_id).decode("utf-8")
-
-
-
-                    context['room_data'] = messages
-                context['user_id'] = ints.name
-                    # context['hidden_str'] = key
+                context['room_id_list'] = roomIdList
+                context['room_data'] = messages
 
         return render(request ,'chat_room.html',context)
 
@@ -120,132 +90,65 @@ class ChatRoomView(View):
         render_context = {}
         render_context['context'] = {}
         render_context['return_type'] = 'template_response'
-        render_context['template'] = 'chatroom/chatroom.html'
+        render_context['template'] = 'chat_room.html'
         render_context['redirect_url'] = reverse('chatroom_view',request)
         return render_context
 
     def put(self,request,*args,**kwargs):
-        print('Put-------------',request.body)
+        content_data = {}
         if request.is_ajax():
-            room = None
-            # room = request.PUT.get('room')
-            # try:
-            #     data = json.loads(request.body)
-            #     room = data.get('room')
-            #     # Your logic for handling the PUT request data here
-            # except json.JSONDecodeError as e:
-            #     print("+++++++++++",e)
-            room = request.GET.get('room')
-            print('Put-------------',room)
-            content_data = {}
-            try:
-                if room is not None:
-                    content_data['active_room'] = room
-                    # room_id = AESRoomEnc(ENC_KEY).decrypt(room,ENC_KEY)
-                    room_id = room
-                    print('Put-------------',room_id)
-                    # exist_user_in_room(request,room_id)
-                    room_chat = Message.objects.filter(room=room_id).order_by('timestamp')
-                    if room_chat:
-                        content_data['error'] = False
-                        participents = list(room_chat.values_list('sender_id', flat=True))
-                        participentsData = { x['id']:{'profile_image':"static/img/user-3.png",'name':x['name']} for x in User.objects.filter(id__in = participents).values('name','id')}
-                        roomChat = { x['id']:{'message':x["content"],'time':x['timestamp'],'sender':x['sender_id']} for x in room_chat.values('id','sender_id','timestamp','content')}
-                        content_data['room_participents'] = participentsData
-                        content_data['room_chat'] = roomChat
-                    else:
-                        content_data['error'] = True
-                return JsonResponse(content_data)
-            except Exception as e:
-                # Handle the error
-                print(e)
-                error_message = "An error occurred: " + str(e)
-                return JsonResponse({'error': error_message}, status=400)
-        return JsonResponse({'error': 'An error occurred:'}, status=400)
-            
-# class ChatDocumentUpload(View):
-    
-#     def get(self,request,*args,**kwargs):
-#         context = {}
-#         # form = ChatRoomDocumentForm()
-#         # button =[
-# 		# 	{'type':"submit" ,'label':_("Save changes"),'class':"btn btn-brand",},
-# 		# ]
-#         # form.buttons = button
-#         # context['form'] = form
-#         document_upload_form
-#         return None
-
-# def chat_document_upload_ajax(request,*args,**kwargs):
-#     print("request",args,kwargs)
-
-#     content_data = {'error': False, 'message': 'Uploaded Successfully','room':"room"}
-#     if request.method == 'POST':
-#         form = ChatRoomDocumentForm(request.POST,request.FILES)
-#         if form.is_valid():
-
-#             print("Form Valid")
-#         else:
-#             print("Form Valid",form.errors)
-#         print("----",request.user)
-#     return JsonResponse(content_data)
+            room_id = request.GET.get('room',None)
+            if room_id is not None:
+                content_data = get_room_chat(room_id)
+                content_data['active_room'] = room_id
+        return JsonResponse(content_data)
 
 class ManageRoom(object):
 
     def update_last_view_message(request, *args, **kwargs):
+        context = {}
         if request.method == 'POST':
             active_room = request.POST.get('room',None) 
             user = request.POST.get('user',None) 
-            if active_room is not None:
-                # room_id = AESRoomEnc(ENC_KEY).decrypt(active_room,ENC_KEY)
-                room_id = active_room
-                ints = User.objects.filter(name=user).first()
-                if ints:
-                    exist_user_in_roomId(ints,room_id)
-                    update_last_seen_message(ints,room_id)
-                    response_data = {'error': False,'success':True}
-                    return JsonResponse(response_data)
+            userInst = User.objects.filter(name=user).first()
+            if active_room and userInst:
+                exist_user_in_roomId(userInst,active_room)
+                update_last_seen_message(userInst,active_room)
+                context = {'success':True}
+            else:
+                context = {'error': 'Room or user does not exist'}
 
-        return JsonResponse({'error': 'Invalid request'})
-
+        return JsonResponse(context)
 class ManageChat(object):
-     
      def insert_message(request, *args, **kwargs):
+        context = {}
         if request.method == 'POST':
             active_room = request.POST.get('room',None) 
             message = request.POST.get('message',None)
-            user = request.POST.get('user',None)
-            ints = None
-            if user:
-                ints = User.objects.get(name=user)
-            if active_room is not None and ints is not None:
-                content_data = {'error': False,'success':True}
-                content_data['active_room'] = active_room
-                # room_id = AESRoomEnc(ENC_KEY).decrypt(active_room,ENC_KEY)
-                room_id = active_room
+            user = request.POST.get('user',None) 
+            userInst = User.objects.filter(name=user).first()
+            try:
+                if active_room and userInst:
+                    context['success'] = True
+                    context['active_room'] = active_room
 
-                # exist_room(room_id)
-                # exist_user_in_room(request,room_id)
+                    msgInstance = Message()
+                    msgInstance.room_id = active_room
+                    msgInstance.content = message
+                    msgInstance.sender_id = userInst.id
+                    msgInstance.save()
 
-                msgInstance = Message()
-                msgInstance.room_id = room_id
-                msgInstance.content = message
-                msgInstance.sender_id = ints.id
-                # msgInstance.timestamp =
-                msgInstance.save()
+                    update_room_last_message(msgInstance.id,active_room)
+                    update_last_seen_message(userInst,active_room)
+                    
+                    participentsData = { userInst.name:{'profile_image':'{% static  "img/user-1.png" %}','name':userInst.name}}
+                    roomChat = { msgInstance.id :{'message':msgInstance.content,'time':msgInstance.timestamp,'sender':userInst.name}}
 
-                update_room_last_message(msgInstance.id,room_id)
-                update_last_seen_message(ints,room_id)
-                # encUid = ENC_KEY.encrypt(str(request.user.id))
-                encUid = ints.name
-                participentsData = { encUid:{'profile_image':'','name':encUid}}
-                roomChat = { msgInstance.id :{'message':msgInstance.content,'time':msgInstance.timestamp,'sender':encUid}}
-
-                content_data['room_participents'] = participentsData
-                content_data['room_chat'] = roomChat
-                
-                return JsonResponse(content_data)
-
-        return JsonResponse({'error': 'Invalid request'})
-
-
+                    context['room_participents'] = participentsData
+                    context['room_chat'] = roomChat
+                else:
+                    context['error'] = 'Room or user does not exist'
+            except Exception as e:
+                print("Exception from insert_message",e)
+                context['error'] = e
+        return JsonResponse(context)
